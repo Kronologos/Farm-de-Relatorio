7924ae556815b2df892b09d22b0690a7bb9ebe29
Nício Murilo da Silva <nicio.silva23@gmail.com>
2021-03-08 20:58:39 -0300

    V1

--- Farm.py
+++ Farm.py
@@ -1,15 +1,20 @@
 import sympy as sp 
 
 class Farm():
-    def __init__(self,Formula,*args):
+    def __init__(self,Formula: str,var: str,*args,linhas=1):
         """
-        Recebe uma string da fórmula, um dict das variaveis fixas e os argumentos
+        Recebe uma string da fórmula, uma string com a variável final e um número
+        variável de listas no qual o primeiro elemento é a variável a ser substituída
+        e o restante é o valor a substituir. Um parâmetro opcional é o número de linhas a serem computadas.
+        Ex: A = Farm('(4/3)*pi*(r^3)', 'V', [ ['r', 1,2,3,4] ])
         """
         self.formula = Formula
+        self.var = var
         self.Dados = self.get_dados(args)
         self.tornar_simbolica()
+        self.faz(linhas)
 
-    def get_dados(self, lista_de_dados):
+    def get_dados(self, lista_de_dados: list):
         '''
         Essa função transforma os dados inseridos em um dicionário onde o index é a variável e o item são os valores.
         ''' 
@@ -19,33 +24,47 @@ class Farm():
         return dic_dados
 
     def tornar_simbolica(self):
+        '''
+        Essa função transforma a fórmula inserida em uma fórmula Sympy.
+        '''
         self.formula = sp.sympify(self.formula)
         
     def formula_ltx(self):
+        '''
+        Essa função retorna a fómula inserida em linguagem LaTeX.
+        '''
         return sp.latex(self.formula)
     
     def faz_simb(self, index):
+        '''
+        Essa função substitui simbolicamente os valores inseridos na fórmula de acordo
+        com seus índices na list de dados, depois iguala ao resultado da substituição.
+        '''
         temp = self.formula.subs({chave:sp.Symbol(str(self.Dados[chave][index])) for chave in self.Dados.keys()})
         temp_num =self.formula.subs({chave:(self.Dados[chave][index]) for chave in self.Dados.keys()})
-        temp = sp.latex(self.formula) + " =" +sp.latex(temp)+" = {0}".format(temp_num)
+        temp =sp.latex(temp)+"= {0}".format(temp_num)+r'\\'+'\n'
         return temp
 
     def substitui_ai_pra_nos(self, linhas = 1):
+        '''
+        Essa função faz a substituição para um número declarado de linhas.
+        ex: substitui_ai_pra_nos(linhas = 4)
+        '''
+        my_str = ''
         n = linhas-1#auxiliar
+        my_str+= self.var +" &= "+ self.formula_ltx()+'\n'
         for i in range(linhas): 
             ltx = self.faz_simb(i)
-            print(ltx)
+            my_str+= self.var+'&='+ltx
+        return my_str
+    
+    def faz(self,linhas=1):
+        print(r'\begin{align}'+'\n')
+        print(self.substitui_ai_pra_nos(linhas=linhas))
+        print(r'end{align}')
+        
 
-#Testezin ra´pido
-s = 's_0 + v_0*t +a*(t/2)^2'
 
-s_in = ['s_0', 1,2,3,4]
-v_in = ['v_0', 1.2, 2, 7, 4.5]
-a = ['a', 23.4, 31, 15, 69]
-t= ['t', 3.6, 4.5,9]
-
-A = Farm(s,s_in,v_in,a,t)
-A.substitui_ai_pra_nos(linhas=2)
 
 
 
